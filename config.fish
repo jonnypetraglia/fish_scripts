7a7ae4b07d24b5d30d5fb3dd0e419a701e05fdcb
Jonny Petraglia <jpetraglia@inspirato.com>
2021-04-22 08:47:39 -0600

    More functions!

--- config.fish
+++ config.fish
@@ -5,7 +5,9 @@ set GIT_ASKPASS '/usr/bin/ksshaskpass'
 set SFDX_MDAPI_TEMP_DIR '/tmp/sfdx-debug'
 mkdir -p $SFDX_MDAPI_TEMP_DIR
 set PATH $PATH $HOME/.gem/ruby/2.7.0/bin
+set ELECTRON_TRASH gio
 
+set SFDX_REST_DEPLOY false
 
 function fish_greeting
     fortune
